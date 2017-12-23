216e7cbe2aa512db2ab0438f48d86d52fdd0e9d6
Zachary Pleasant <zpleasant10@gmail.com>
2017-12-23 13:53:50 -0600

    added ability to log user choice from radio buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,18 +19,24 @@ $(document).ready(function(){
   var answer10 = "d";
 
   // define variables for user answers
-  var userAnswer1 = ; // need to populate these...
-  var userAnswer2 = ;
-  var userAnswer3 = ;
-  var userAnswer4 = ;
-  var userAnswer5 = ;
-  var userAnswer6 = ;
-  var userAnswer7 = ;
-  var userAnswer8 = ;
-  var userAnswer9 = ;
-  var userAnswer10 = ;
+  var userAnswer1 = ""; // need to populate these...
+  var userAnswer2 = "";
+  var userAnswer3 = "";
+  var userAnswer4 = "";
+  var userAnswer5 = "";
+  var userAnswer6 = "";
+  var userAnswer7 = "";
+  var userAnswer8 = "";
+  var userAnswer9 = "";
+  var userAnswer10 = "";
 
   // define functions
+
+  // function that grabs the user choice
+  $("input[type='button']").click(function(){
+    var userAnswer1 = $("input[name='q1']:checked").val();
+    console.log(userAnswer1);
+  });
 
   // on click that starts game when user clicks start button
   $(".start").click(function() {
@@ -40,7 +46,26 @@ $(document).ready(function(){
 
   // on click function that ends game with user clicks submit button
   $(".submit").click(function(){
-    console.log("user has ended the game");
+    userAnswer1 = $("input[name='q1']:checked").val();
+    console.log(userAnswer1);
+    userAnswer2 = $("input[name='q2']:checked").val();
+    console.log(userAnswer2);
+    userAnswer3 = $("input[name='q3']:checked").val();
+    console.log(userAnswer3);
+    userAnswer4 = $("input[name='q4']:checked").val();
+    console.log(userAnswer4);
+    userAnswer5 = $("input[name='q5']:checked").val();
+    console.log(userAnswer5);
+    userAnswer6 = $("input[name='q6']:checked").val();
+    console.log(userAnswer6);
+    userAnswer7 = $("input[name='q7']:checked").val();
+    console.log(userAnswer7);
+    userAnswer8 = $("input[name='q8']:checked").val();
+    console.log(userAnswer8);
+    userAnswer9 = $("input[name='q9']:checked").val();
+    console.log(userAnswer9);
+    userAnswer10 = $("input[name='q10']:checked").val();
+    console.log(userAnswer10);
     // finish the game on a user click
   });
 
